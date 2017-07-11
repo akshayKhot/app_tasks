cd7856f5359c7c32bb0996a0c7c52a923398d445
Akshay Khot <akshay.7277@gmail.com>
2017-07-11 15:57:14 -0700

    get data from db

--- server.js
+++ server.js
@@ -1,43 +1,31 @@
 const express = require('express')
 const app = express()
-var _ = require('lodash');
+var _ = require('lodash')
+var pgp = require('pg-promise')()
+
+var cn = {
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres'
+}
+var db = pgp(cn)
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
 })
-
-var tasks = [{
-    task: "Workout",
-    deadline: "5.30 pm",
-    date: "10072017"
-}, {
-    task: "Make Coffee",
-    deadline: "6.30 pm",
-    date: "10072017"
-}, {
-    task: "Go to bed",
-    deadline: "10.30 pm",
-    date: "09072017"
-}, {
-    task: "Wake up early",
-    deadline: "6.30 am",
-    date: "10072017"
-}];
 
 app.get('/api/tasks/:date', (req, res) => {
     console.log("Received call");
-    var date = req.params.date;
-    var tasks_for_date = _.filter(tasks, task => task.date == date);
-    res.json(tasks_for_date);
+    db.query("select * from tasks")
+    .then(db_tasks => {
+        var date = req.params.date;
+        var tasks_for_date = _.filter(db_tasks, task => task.date == date);
+        res.json(tasks_for_date);
+    })
+    .catch(err => console.log(err));
 })
-
-
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
