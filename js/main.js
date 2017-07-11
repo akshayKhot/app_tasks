d6cdc0d0b8f2cfc118903b4f7e441974476a5d1b
Akshay Khot <akshay.7277@gmail.com>
2017-07-11 12:06:43 -0700

    fill out div dynamically

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 console.log("Hello World");
 $.getJSON("http://localhost:3000/api/tasks/10072017", tasks => {
-    // _.forEach(tasks, task => {
-    //     $(".task").append("<h4>task</h4>");
-    // })
-
-    $(".task").each(index => {
-        $(this).text("Hi");
+    $(".task").each(function(index) {
+        $(this).html("<h4>" + tasks[index].task + "</h4>");
+    });
+    $(".dl").each(function(index) {
+        $(this).html("<h4>" + tasks[index].date + "</h4>");
     });
 });
