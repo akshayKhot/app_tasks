5479850c9a8f2d2d8f55139370a8a983156311bb
Akshay Khot <akshay.7277@gmail.com>
2017-07-11 12:29:49 -0700

    add moment and date

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 console.log("Hello World");
 $.getJSON("http://localhost:3000/api/tasks/10072017", tasks => {
+
+    var dateString = tasks[0].date;
+    var formattedDate = moment(dateString, "DDMMYYYY").format("MMMM Do YYYY");
+    $("#date").html(formattedDate);
+
     $(".task").each(function(index) {
         $(this).html("<h4>" + tasks[index].task + "</h4>");
     });
-    $(".dl").each(function(index) {
-        $(this).html("<h4>" + tasks[index].date + "</h4>");
+    $(".dl").each(function(index) {    
+        $(this).html("<h4>" + tasks[index].deadline + "</h4>");
     });
 });
